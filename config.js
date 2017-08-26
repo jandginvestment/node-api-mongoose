44327c6fe6e596815b17377674906484dc1660f3
jandg <jandginvestment@gmail.com>
2017-08-26 23:35:59 +0400

    co

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://theese:test54321@ds159953.mlab.com:59953/vivasayee'
+    'database': 'mongodb://theese:test54321@ds159953.mlab.com:59953/puse'
 }; 
